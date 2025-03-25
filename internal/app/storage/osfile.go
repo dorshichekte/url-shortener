c18f050d35669ab8b2944188e468fe31a0a72610
dorshichekte <vdpntr@uandex.ru>
2025-03-26 00:00:39 +0300

    fix(internal): iter9 try to fix pr

--- internal/app/storage/osfile.go
+++ internal/app/storage/osfile.go
@@ -7,14 +7,8 @@ import (
 	"os"
 )
 
-const fileName = "url-db.json"
-
 func NewConsumer(filePath string) (*Consumer, error) {
-	if filePath[len(filePath)-1] != '/' {
-		filePath += "/"
-	}
-
-	file, err := os.OpenFile(filePath+fileName, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0755)
+	file, err := os.OpenFile(filePath, os.O_WRONLY|os.O_CREATE|os.O_APPEND, 0644)
 	if err != nil {
 		return nil, err
 	}
@@ -34,18 +28,12 @@ func (c *Consumer) Close() error {
 }
 
 func NewProducer(filePath string) (*Producer, error) {
-	createFile(filePath)
-
-	if filePath[len(filePath)-1] != '/' {
-		filePath += "/"
-	}
-
-	file, err := os.OpenFile(filePath+fileName, os.O_RDONLY|os.O_CREATE, 0755)
+	file, err := os.OpenFile(filePath, os.O_RDONLY|os.O_CREATE, 0644)
 	if err != nil {
 		return nil, err
 	}
 
-	log.Printf("Файл для хранения данных URL создан по пути: %s", filePath+fileName)
+	log.Printf("Файл для хранения данных URL создан по пути: %s", filePath)
 
 	return &Producer{
 		file:    file,
@@ -64,10 +52,4 @@ func (p *Producer) ReadEvent(s *URLStorage) (*Event, error) {
 
 func (p *Producer) Close() error {
 	return p.file.Close()
-}
-
-func createFile(filePath string) {
-	if _, err := os.Stat(filePath); os.IsNotExist(err) {
-		os.MkdirAll(filePath, 0755)
-	}
 }
