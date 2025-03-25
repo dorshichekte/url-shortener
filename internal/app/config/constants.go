e85a451aef670979ad12fa45f710fc79a450872f
dorshichekte <vdpntr@uandex.ru>
2025-03-25 15:07:56 +0300

    iter9 fix pr

--- internal/app/config/constants.go
+++ internal/app/config/constants.go
@@ -3,5 +3,5 @@ package config
 const (
 	DefaultAddress             = "localhost:8080"
 	DefaultAddressWithProtocol = "http://localhost:8080"
-	StoragePath                = "url-db.json"
+	StoragePath                = "/tmp/url-db.json"
 )
