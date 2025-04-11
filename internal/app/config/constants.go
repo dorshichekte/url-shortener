f7c827deeb34c2f1f1694a0f21cec6823f3c8ed5
dorshichekte <vdpntr@uandex.ru>
2025-04-11 17:54:19 +0300

    features(internal): iter 13, add error

--- internal/app/config/constants.go
+++ internal/app/config/constants.go
@@ -3,5 +3,5 @@ package config
 const (
 	DefaultAddress             = "localhost:8080"
 	DefaultAddressWithProtocol = "http://localhost:8080"
-	DefaultStoragePath         = "url-db.json"
+	DefaultStoragePath         = "file-db.json"
 )
